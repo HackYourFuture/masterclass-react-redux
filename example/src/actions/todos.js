446f0a3cd90e67d69a99b247512fcabb91f0d3fe
Iris Salcedo <irissalcedo@protonmail.com>
2017-04-23 01:28:52 +0200

    redux: reset work

--- example/src/actions/todos.js
+++
@@ -1,9 +0,0 @@
-export const PREFIX = 'TODOS_';
-export const ADD_TODO = PREFIX + 'ADD_TODO';
-export const REMOVE_TODO = PREFIX + 'REMOVE_TODO';
-export const CHANGE_TODO = PREFIX + 'CHANGE_TODO';
-
-
-export const addTodo = (todo) => ({type: ADD_TODO, todo})
-export const removeTodo = (index) => ({type: REMOVE_TODO, index})
-export const changeTodo = (index, description) => ({type: CHANGE_TODO, description})
